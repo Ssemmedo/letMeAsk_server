a237b341884889227a892c128c345538caac7dea
Sandro Semedo <ssemdo44@gmail.com>
2025-07-09 18:10:58 -0100

    bug: import not used

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,4 @@
 import { fastifyCors } from '@fastify/cors'
-import { sql } from './db/connection.ts'
 import { fastify } from 'fastify'
 import {
   serializerCompiler,
@@ -7,10 +6,10 @@ import {
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
 import { env } from './env.ts'
-import { getRoomsRoute } from './http/routes/get-rooms.ts'
+import { createQuestionRoute } from './http/routes/create-question.ts'
 import { createRoomRoute } from './http/routes/create-room.ts'
 import { getRoomQuestions } from './http/routes/get-room-questions.ts'
-import { createQuestionRoute } from './http/routes/create-question.ts'
+import { getRoomsRoute } from './http/routes/get-rooms.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
