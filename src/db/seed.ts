02b11f6d1c33baa6cbd313030c12fff8e0945c08
Sandro Semedo <ssemdo44@gmail.com>
2025-07-09 18:07:13 -0100

    feat: adding routes

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,21 +1,25 @@
-import{reset, seed} from 'drizzle-seed'
-import { schema } from './schema/index.ts'
+import { reset, seed } from 'drizzle-seed'
 import { db, sql } from './connection.ts'
+import { schema } from './schema/index.ts'
 
 await reset(db, schema)
 
 await seed(db, schema).refine((f) => {
-    return{
-        rooms: {
-            count: 20,
-            columns: {
-                name: f.companyName(),
-                description: f.loremIpsum(),
-            },
-        },
-    }
+  return {
+    rooms: {
+      count: 20,
+      columns: {
+        name: f.companyName(),
+        description: f.loremIpsum(),
+      },
+      with: {
+        questions: 5,
+      },
+    },
+  }
 })
 
 await sql.end()
 
+// biome-ignore lint/suspicious/noConsole: <Only used in development>
 console.log('database seeded')
